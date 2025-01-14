b672f119b5b86cde01f38103d0f502fe1b9a6d97
vocongbinh <binh93phudong@gmail.com>
2025-01-14 11:57:22 +0700

    fix redis connect

--- src/queue.ts
+++ src/queue.ts
@@ -1,10 +1,14 @@
 import { Queue } from 'bullmq';
 import Redis from 'ioredis';
-const redis = new Redis( {
-    host: 'oregon-redis.render.com',
-    port: 6379,
-    username: 'red-cu2ifhpu0jms73d9icng',
-    password: 'dbgs9sD1PB90NjiWGNvGyK0fJsYdEujj',
-    tls: {} 
+const redisUrl = 'rediss://red-cu2ifhpu0jms73d9icng:dbgs9sD1PB90NjiWGNvGyK0fJsYdEujj@oregon-redis.render.com:6379';
+const parsedUrl = new URL(redisUrl);
+
+const redis = new Redis({
+    host: parsedUrl.hostname,
+    port: Number(parsedUrl.port),
+    username: parsedUrl.username,
+    password: parsedUrl.password,
+    tls: parsedUrl.protocol === 'rediss:' ? {} : undefined,
+    maxRetriesPerRequest: null
 });
 export const queue = new Queue('contract', {connection: redis});
--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,17 @@
 import { Worker } from 'bullmq';
 import Redis from 'ioredis';
 import { deployNFT } from './jobs';
+
+const redisUrl = 'rediss://red-cu2ifhpu0jms73d9icng:dbgs9sD1PB90NjiWGNvGyK0fJsYdEujj@oregon-redis.render.com:6379';
+const parsedUrl = new URL(redisUrl);
+
 const redis = new Redis({
-    host: 'oregon-redis.render.com',
-    port: 6379,
-    username: 'red-cu2ifhpu0jms73d9icng',
-    password: 'dbgs9sD1PB90NjiWGNvGyK0fJsYdEujj',
-    tls: {} 
+    host: parsedUrl.hostname,
+    port: Number(parsedUrl.port),
+    username: parsedUrl.username,
+    password: parsedUrl.password,
+    tls: parsedUrl.protocol === 'rediss:' ? {} : undefined,
+    maxRetriesPerRequest: null
 });
 const worker = new Worker('contract', async job => {
     await deployNFT(job.data);
