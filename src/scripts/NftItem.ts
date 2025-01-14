fcd602b957c835db039fb97ce0bc23afac1389e1
vocongbinh <binh93phudong@gmail.com>
2025-01-14 14:40:58 +0700

    fix

--- src/scripts/NftItem.ts
+++ src/scripts/NftItem.ts
@@ -40,7 +40,7 @@ export class NftItem {
         ],
         sendMode: SendMode.IGNORE_ERRORS + SendMode.PAY_GAS_SEPARATELY,
       });
-      await sleep(4000)
+      await sleep(8000)
       const nftAddress = await NftCollection.getNftAddressByIndex(params.itemIndex)
       console.log(nftAddress.toString())
       const newOwner = Address.parse(destAddress)
